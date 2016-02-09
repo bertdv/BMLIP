09389db9b8f06c29f2b877075340a6c3715c459d
Marco Cox <mail@marcocox.com>
2016-02-09 17:16:08 +0100

    Finish pPCA demo

--- lessons/11_continuous_latent_variable_models/scripts/pca_demo_helpers.jl
+++ lessons/11_continuous_latent_variable_models/scripts/pca_demo_helpers.jl
@@ -21,12 +21,21 @@ end
 function pPCA(X::Matrix, M::Int64)
     # Implement probabilistic PCA using the EM algorithm]𝜓
     # X collects the data points on its columns
-    _X = copy(X)
-    N = size(_X, 2); D = size(_X, 1)
+    # X may contain missing values, indicated by NaN
+    N = size(X, 2); D = size(X, 1)
 
-    # Shift _X to be zero-mean
-    μ = (sum(_X, 2) / N)[:,1] # [:,1] makes sure this is a Vector
-    _X = _X .- μ
+    # Shift X to be zero-mean, ignoring missing values
+    # Missing values are treated as latent variables,
+    # so we keep track of E[x-μ] and E[(x-μ)' * (x-μ)].
+    # In case there are no missing values, both expectations are constants.
+    missing_values = isnan(X)
+    has_missing_values = sum(missing_values) > 0
+    e_X = copy(X) # E[X.-μ]
+    e_X[missing_values] = 0.0 # set missing values to 0.0 to ignore them in mean calculation
+    μ = ( sum(e_X, 2) ./ (N*ones(D)-sum(missing_values,2)) )[:,1] # make sure missing values don't influence the mean. [:,1] makes sure this is a Vector
+    e_X = X .- μ
+    e_X[missing_values] = 0.0 # Initialize expected value of missing values to the mean
+    e_xx = [(e_X[:,n]'*e_X[:,n])[1,1] for n=1:N] # E[(x-μ)' * (x-μ)]
 
     # Initialize variables
     θ = pPCAParams(Matrix{Float64}(D,M), μ, 0.0) # θ holds the parameters
@@ -36,16 +45,29 @@ function pPCA(X::Matrix, M::Int64)
         e_zz[n] = e_z[n] * e_z[n]'
     end
 
-    function eStep!(e_z, e_zz)
+    function eStep!(e_z, e_zz, e_X, e_xx)
         # Update e_z and e_zz
         W = θ.W; σ2 = θ.σ2
         R = W'*W + σ2*eye(M)
         K = inv(R) * W'
         C = σ2*inv(R)
 
+        e_X[missing_values] = 0.0 # ignore missing values in calculation of e_z
         for n=1:N
-            e_z[n] = K * _X[:,n]
+            e_z[n] = K * e_X[:,n]
             e_zz[n] = C + e_z[n] * e_z[n]'
+        end
+
+        if has_missing_values
+            # Update e_X and e_xx
+            for idx in find(missing_values)
+                n = Int(ceil(idx / D)) # index of data point
+                d = (idx-1)%D + 1 # dimension
+                e_X[d,n] = (W[d,:] * e_z[n])[1,1]
+            end
+            for n in find(sum(missing_values,1)) # every datapoint with at least 1 missing value
+                e_xx[n] = (e_X[:,n]'*e_X[:,n])[1,1] + sum(missing_values[:,n])*σ2
+            end
         end
     end
 
@@ -53,7 +75,7 @@ function pPCA(X::Matrix, M::Int64)
         # Update W and σ2
         W1 = zeros(D,M)
         for n=1:N
-            W1 += _X[:,n] * e_z[n]'
+            W1 += e_X[:,n] * e_z[n]'
         end
         W = W1 * inv(sum(e_zz))
         θ.W = W
@@ -61,14 +83,14 @@ function pPCA(X::Matrix, M::Int64)
         accum = 0.0
         W2 = W'*W
         for n=1:N
-            accum += (_X[:,n]'*_X[:,n] - 2 * e_z[n]' * W' *_X[:,n] + trace(e_zz[n]*W2))[1,1]
+            accum += (e_xx[n] - 2 * e_z[n]' * W' *e_X[:,n] + trace(e_zz[n]*W2))[1,1]
         end
         θ.σ2 = accum / (N*D) # update σ2
     end
 
-    for i=1:20 # Here should be a decent convergence check, but hey, this also works...
+    for i=1:30 # Here should be a decent convergence check, but hey, this also works...
         mStep!(θ)
-        eStep!(e_z, e_zz)
+        eStep!(e_z, e_zz, e_X, e_xx)
     end
 
     # Collect E[z_1],...,E[z_N] in a matrix
