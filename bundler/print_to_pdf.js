5fd3258ea95449885f25936fc070a2c29d8c69dd
Joris Kraak <jkraak@gnresound.com>
2016-02-11 17:01:29 +0100

    Add table of contents generation
    The links and back-links do not currently seem to work, they still point
    to the 'original' document from which the PDF is generated. When
    generating the PDF in Chrome instead of using PhantomJS, the links keep
    working in return for rendering errors.

--- bundler/print_to_pdf.js
+++ bundler/print_to_pdf.js
@@ -1,9 +1,61 @@
-var webpage = require('webpage');
-
+var fs = require('fs');
 var system = require('system');
-var url = system.args[1];
+var toc = require('toc');
+var webpage = require('webpage');
+var page = webpage.create();
 
-var capture = function capture(page, pageUrl, callback) {
+var uri = system.args[1];
+
+var html = fs.read(uri, { mode: 'r', charset: 'utf-8' });
+var toc_options = {
+  anchorMax: 3,
+  anchorMin: 1,
+  header: '<h<%= level %><%= attrs %>><a href="#toc-<%= anchor %>" name="<%= anchor %>"><%= header %></a></h<%= level %>>',
+  // Need to modify the text to get rid of the paragraph sign at the end of it
+  openLI: '<li><a href="#<%= anchor %>" name="toc-<%= anchor %>"><%= text.substr(0, text.length - 3) %></a>',
+  tocMax: 3,
+  tocMin: 1
+};
+fs.write('bundle/bundle_with_toc.html', toc.process(html, toc_options), {
+  mode: 'w',
+  charset: 'utf-8'
+});
+
+page.onError = function(msg, trace) {
+  // Suppress IPython not found messages...
+  if(!/IPython/.test(msg)) {
+    console.error('Error:', msg);
+    trace.forEach(function(item) {
+      console.error('  ', item.file, ':', item.line);
+    });
+  }
+};
+
+page.paperSize = {
+  footer: {
+    contents: phantom.callback(function(pageNum, numPages) {
+      if(pageNum > 1) {
+        return '<span style="float: right; font-size: 75%">' + pageNum + '/' + numPages + '</span>';
+      } else {
+        return '';
+      }
+    }),
+    height: '1.2cm',
+  },
+  format: 'A4'
+};
+
+capture(page, 'bundle/bundle_with_toc.html', function(error) {
+  if(error) {
+    console.error('Error', error);
+    phantom.exit(1);
+  } else {
+    page.render('AIP-5SSB0.pdf', { format: 'pdf' });
+    phantom.exit(0);
+  }
+});
+
+function capture(page, pageUrl, callback) {
   page.open(pageUrl, function(status) {
     var interval, allDone;
 
@@ -40,36 +92,3 @@ var capture = function capture(page, pageUrl, callback) {
     }, 100);
   });
 };
-
-var page = webpage.create();
-
-page.onError = function(msg, trace) {
-  console.error(msg);
-  trace.forEach(function(item) {
-    console.error('  ', item.file, ':', item.line);
-  });
-};
-
-page.paperSize = {
-  footer: {
-    contents: phantom.callback(function(pageNum, numPages) {
-      if(pageNum > 1) {
-        return '<span style="float: right; font-size: 75%">' + pageNum + '/' + numPages + '</span>';
-      } else {
-        return '';
-      }
-    }),
-    height: '1.2cm',
-  },
-  format: 'A4'
-};
-
-capture(page, system.args[1], function(error) {
-  if(error) {
-    console.error(error);
-    phantom.exit(1);
-  } else {
-    page.render('AIP-5SSB0.pdf', { format: 'pdf' });
-    phantom.exit(0);
-  }
-});
