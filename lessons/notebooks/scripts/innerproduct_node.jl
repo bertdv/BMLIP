57b468aa033f369ce8f016d9cecb1c90ca544bd8
Marco Cox <mail@marcocox.com>
2018-01-30 11:26:04 +0100

    Update lesson 12 to ForneyLab v0.7

--- lessons/notebooks/scripts/innerproduct_node.jl
+++ lessons/notebooks/scripts/innerproduct_node.jl
@@ -1,109 +1,140 @@
-# This file implements the DotProductNode and corresponding sumProductRule! methods
+# This file implements the DotProductNode and corresponding sum-product rules
 
-using ForneyLab # v0.4.0
+using ForneyLab # v0.7.0
 
-export DotProductNode
-import ForneyLab.sumProductRule!
+import ForneyLab: DeltaFactor, Univariate, Multivariate, generateId, associate!, addNode!, ensureParameters!
+export DotProduct
 
 """
 Description:
 
     out = β⋅in
 
-    in:  d-dimensional vector
-    β:   d-dimensional vector
+    in1: d-dimensional vector
+    in2: d-dimensional vector
     out: scalar
 
-           β
+           in2
            |
-      in   V   out
+      in1  V   out
     ----->[⋅]----->
 
-    f(in, β, out) =  δ(out - β⋅in)
+    f(in1, in2, out) =  δ(out - in1'*in2)
 
 Interfaces:
 
-    1 i[:in], 2 i[:β], 3 i[:out]
+    1 i[:in1], 2 i[:in2], 3 i[:out]
 
 Construction:
 
-    DotProductNode(id=:my_node)
+    DotProduct(out, in1, in2, id=:my_node)
 """
-type DotProductNode <: Node
+type DotProduct <: DeltaFactor
     id::Symbol
-    interfaces::Array{Interface,1}
+    interfaces::Vector{Interface}
     i::Dict{Symbol,Interface}
 
-    function DotProductNode(; id=ForneyLab.generateNodeId(DotProductNode))
-        self = new(id, Array(Interface, 3), Dict{Symbol,Interface}())
+    function DotProduct(out::Variable, in1::Variable, in2::Variable; id=generateId(DotProduct))
+        self = new(id, Array(Interface, 3), Dict{Int,Interface}())
         addNode!(currentGraph(), self)
-
-        for (id, name) in enumerate([:in, :β, :out])
-            self.i[name] = self.interfaces[id] = Interface(self)
-        end
+        self.i[:out] = self.interfaces[1] = associate!(Interface(self), out)
+        self.i[:in1] = self.interfaces[2] = associate!(Interface(self), in1)
+        self.i[:in2] = self.interfaces[3] = associate!(Interface(self), in2)
 
         return self
     end
 end
 
-isDeterministic(::DotProductNode) = true
+slug(::Type{DotProduct}) = "dot"
 
 
-"""
-DotProductNode:
+# Update rule definitions
 
-             β
-             | ↓ N{d}
-             |
-             V
-    x ----->[⋅]-----> y
-        →        →
-       δ{d}      N
-"""
-function sumProductRule!{dims}( node::DotProductNode,
-                                outbound_interface_index::Type{Val{3}},
-                                y::Gaussian,
-                                msg_1::Message{MvDelta{Float64,dims}},
-                                msg_2::Message{MvGaussian{dims}},
-                                ::Any)
+#             β
+#             | ↓ N{d}
+#             |
+#             V
+#    x ----->[⋅]-----> y
+#        →        →
+#       δ{d}      N
 
-    x = msg_1.payload.m # We'll call in x
-    β = ensureParameters!(msg_2.payload, (:m, :V))
+@sumProductRule(:node_type     => DotProduct,
+                :outbound_type => Message{Gaussian},
+                :inbound_types => (Void, Message{PointMass}, Message{Gaussian}),
+                :name          => SPDotProductOutVPG)
 
-    y.m = (x' * β.m)[1,1]
-    y.V = (x' * β.V * x)[1,1]
-    y.xi = NaN; y.W = NaN
+function ruleSPDotProductOutVPG(msg_out::Void,
+                            msg_in1::Message{PointMass, Multivariate},
+                            msg_in2::Message{Gaussian, Multivariate})
+    x = msg_in1.dist.params[:m]
+    β = ensureParameters!(msg_in2.dist, (:m, :V))
+    out_m = x' * β.m
+    out_v = x' * β.V * x
 
-    return y
+    return Message(Multivariate, Gaussian, m=out_m, v=out_v)
+
 end
 
+#             β
+#             | ↓ δ{d}
+#             |
+#             V
+#    x ----->[⋅]-----> y
+#        →        →
+#       N{d}      N
 
-"""
-DotProductNode:
+@sumProductRule(:node_type     => DotProduct,
+                :outbound_type => Message{Gaussian},
+                :inbound_types => (Void, Message{Gaussian}, Message{PointMass}),
+                :name          => SPDotProductOutVGP)
 
-             β
-             | ↑ N{d}
-             |
-             V
-    x ----->[⋅]-----> y
-        →        ←
-       δ{d}      N
-"""
-function sumProductRule!{dims}( node::DotProductNode,
-                                outbound_interface_index::Type{Val{2}},
-                                β::MvGaussian{dims},
-                                msg_1::Message{MvDelta{Float64,dims}},
-                                ::Any,
-                                msg_3::Message{Gaussian})
+function ruleSPDotProductOutVGP(msg_out::Void,
+                            msg_in1::Message{Gaussian, Multivariate},
+                            msg_in2::Message{PointMass, Multivariate})
+    SPDotProductOutVGP(msg_out, msg_in2, msg_in1)
+end
 
-    x = msg_1.payload.m # We'll call in x
+#             β
+#             | ↑ N{d}
+#             |
+#             V
+#    x ----->[⋅]-----> y
+#        →        ←
+#       δ{d}      N
+
+@sumProductRule(:node_type     => DotProduct,
+                :outbound_type => Message{Gaussian},
+                :inbound_types => (Message{Gaussian}, Message{PointMass}, Void),
+                :name          => SPDotProductIn2GPV)
+
+function ruleSPDotProductIn2GPV(msg_out::Message{Gaussian, Univariate},
+                            msg_in1::Message{PointMass, Multivariate},
+                            msg_in2::Void)
+    x = msg_in1.dist.params[:m] # We'll call in1 x
     d = length(x)
 
-    y = ensureParameters!(msg_3.payload, (:xi, :W))
-    β.xi = x * y.xi
-    β.W = x * y.W * x'
-    invalidate!(β.m)
-    invalidate!(β.V)
+    y = ensureParameters!(msg_out.dist, (:xi, :w))
+    xi = x * y.params[:xi]
+    w = x * y.params[:w] * x'
 
-    return β
+    return Message(Multivariate, Gaussian, xi=xi, w=w)
+end
+
+#             β
+#             | ↓ δ{d}
+#             |
+#             V
+#    x ----->[⋅]-----> y
+#        ←        ←
+#       N{d}      N
+
+@sumProductRule(:node_type     => DotProduct,
+                :outbound_type => Message{Gaussian},
+                :inbound_types => (Message{Gaussian}, Void, Message{PointMass}),
+                :name          => SPDotProductIn1GVP)
+
+function ruleSPDotProductIn1GVP(msg_out::Message{Gaussian, Univariate},
+                            msg_in1::Void,
+                            msg_in2::Message{PointMass, Multivariate})
+    SPDotProductIn2GPV(msg_out, msg_in2, msg_in1)
 end
