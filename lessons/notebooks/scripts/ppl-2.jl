038d1cec2fe4f9e981acaae9138091e77fd66a8f
wmkouw <wmkouw@gmail.com>
2019-11-21 13:03:01 +0100

    Update to PPL-2. Started PPL-4.

---
+++ lessons/notebooks/scripts/ppl-2.jl
@@ -0,0 +1,29 @@
+using Turing
+using Distributions
+using Plots
+pyplot()
+
+function predict(x, y, w, μ1, μ2)
+    "Compute posterior probabilities of samples under model"
+
+    # Probability under cluster 1
+    logp1 = log(w[1]) + logpdf(MvNormal(μ1, 1.), [x, y])
+
+    # Probability under cluster 2
+    logp2 = log(w[2]) + logpdf(MvNormal(μ2, 1.), [x, y])
+
+    # Add probabilities using log-sum-exp trick for numeric stability.
+    return Turing.logaddexp(logp1, logp2)
+end
+
+function plot_posteriors(X, μ1_estimated, μ2_estimated)
+
+    # Show the data
+    scatter(X[1,:], X[2,:], legend=false, title="Synthetic Dataset")
+
+    # Overlay the posterior probability
+    x1 = range(-5, stop=3)
+    x2 = range(-5, stop=4)
+    contour!(x1, x2, (x, y) -> predict(x, y, [0.5, 0.5], μ1_estimated, μ2_estimated))
+    # colorbar()
+end
