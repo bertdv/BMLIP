af6745aa9c27cd19b513029d75d8cfa2a20d133e
wmkouw <wmkouw@gmail.com>
2019-12-04 16:00:13 +0100

    Fixed error for 2D GMM demo.

--- lessons/notebooks/scripts/ppl-2.jl
+++ lessons/notebooks/scripts/ppl-2.jl
@@ -3,27 +3,24 @@ import Distributions: MvNormal, logpdf
 using Plots
 pyplot()
 
-function predict(x, y, w, μ1, μ2)
+function predict(x, y, pi, μ1, μ2, Σ1, Σ2)
     "Compute posterior probabilities of samples under model"
 
     # Probability under cluster 1
-    logp1 = log(w[1]) + logpdf(MvNormal(μ1, 1.), [x, y])
+    logp1 = log(pi[1]) + logpdf(MvNormal(μ1, Σ1), [x, y])
 
     # Probability under cluster 2
-    logp2 = log(w[2]) + logpdf(MvNormal(μ2, 1.), [x, y])
+    logp2 = log(pi[2]) + logpdf(MvNormal(μ2, Σ2), [x, y])
 
     # Add probabilities using log-sum-exp trick for numeric stability.
     return logaddexp(logp1, logp2)
 end
 
-function plot_posteriors(X, μ1_estimated, μ2_estimated)
+function plot_posteriors(X, μ1, μ2, Σ1, Σ2; x1=range(-5, stop=3), x2=range(-5, stop=4))
 
     # Show the data
-    scatter(X[1,:], X[2,:], legend=false, title="Synthetic Dataset")
+    scatter(X[:,1], X[:,2], legend=false, title="Synthetic Dataset")
 
     # Overlay the posterior probability
-    x1 = range(-5, stop=3)
-    x2 = range(-5, stop=4)
-    contour!(x1, x2, (x, y) -> predict(x, y, [0.5, 0.5], μ1_estimated, μ2_estimated))
-    # colorbar()
+    contour!(x1, x2, (x, y) -> predict(x, y, [0.5, 0.5], μ1, μ2, Σ1, Σ2))
 end
