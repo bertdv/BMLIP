f2f0bf41db9f990caecc64846e1aa36f64badfc3
wmkouw <wmkouw@gmail.com>
2020-01-10 20:52:21 +0100

    Finished variational part of intro

--- lessons/notebooks/scripts/pp-1.jl
+++ lessons/notebooks/scripts/pp-1.jl
@@ -5,6 +5,7 @@ Last update: 09-01-2020
 """
 
 using SpecialFunctions: gamma
+using Distributions: Normal, Gamma
 using Plots
 pyplot()
 
@@ -13,15 +14,44 @@ function prob_normalgamma(μ::Number, τ::Number; m=0, l=1, a=1, b=1)
     return b^a*sqrt(l)/(gamma(a)*sqrt(2*pi)) * τ^(a - 1/2) * exp(-b*τ) * exp( -(l*τ*(μ - m)^2)/2)
 end
 
-function plot_normalgamma(m, l, a, b; x1=range(-5, stop=5), x2=range(0.1, stop=5), overlay=false)
-    "Contour plot of distribution with given parameters."
+function prob_normal_gamma(μ::Number, τ::Number; m=0, v=1, a=1, b=1)
+    "Compute probability of sample under a bivariate [Normal, Gamma] distribution."
+    
+    # Probablity under Normal distribution (Distributions.jl uses mean-standard-deviation parameterization)
+    pμ = 1/(sqrt(2*pi*v))*exp(-(μ - m)^2/(2*v))
+    
+    # Probablity under Gamma distribution (Distributions.jl uses shape-scale parameterization)
+    pτ = b^a / gamma(a)*τ^(a-1)*exp(-b*τ)
+    
+    # Return product of marginal distributions
+    return pμ * pτ
+end
+
+function plot_normalgamma(m, l, a, b; label=nothing, x1=range(-5, stop=5), x2=range(0.1, stop=5), overlay=false)
+    "Contour plot of Normal-Gamma distribution with given parameters."
 
     if overlay
         # Overlay the posterior probability
-        contour!(x1, x2, (x, y) -> prob_normalgamma(x, y, m=m, l=l, a=a, b=b), cbar=nothing)
+        contour!(x1, x2, (x, y) -> prob_normalgamma(x, y, m=m, l=l, a=a, b=b), label=label, cbar=nothing)
     else
         # Overlay the posterior probability
-        contour(x1, x2, (x, y) -> prob_normalgamma(x, y, m=m, l=l, a=a, b=b), cbar=nothing)
+        contour(x1, x2, (x, y) -> prob_normalgamma(x, y, m=m, l=l, a=a, b=b), label=label, cbar=nothing)
+    end        
+    
+    # Axis properties
+    xlabel!("μ")
+    ylabel!("τ")
+end
+
+function plot_normal_gamma(m_μ, v_μ, a_τ, b_τ; label=nothing, color=nothing, x1=range(-5, stop=5), x2=range(0.1, stop=5), overlay=false)
+    "Contour plot of bivariate [Normal, Gamma] distribution with given parameters."
+
+    if overlay
+        # Overlay the posterior probability
+        contour!(x1, x2, (x, y) -> prob_normal_gamma(x, y, m=m_μ, v=v_μ, a=a_τ, b=b_τ), label=label, color=color, cbar=nothing)
+    else
+        # Overlay the posterior probability
+        contour(x1, x2, (x, y) -> prob_normal_gamma(x, y, m=m_μ, v=v_μ, a=a_τ, b=b_τ), label=label, color=color, cbar=nothing)
     end        
     
     # Axis properties
