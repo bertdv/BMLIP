a61cb96135a72e310d7d3caebdbbfdfb27202b82
wmkouw <wmkouw@gmail.com>
2020-01-15 22:13:06 +0100

    Updated pp-2-sampling

--- lessons/notebooks/scripts/pp-2.jl
+++ lessons/notebooks/scripts/pp-2.jl
@@ -6,28 +6,35 @@ Last update: 07-01-2020
 
 import Turing: logaddexp
 import Distributions: MvNormal, logpdf
+import MCMCChains: Chains
 using Plots
 pyplot()
 
-function predict(x, y, pi, μ1, μ2, Σ1, Σ2)
-    "Compute posterior probabilities of samples under model."
-
-    # Probability under cluster 1
-    logp1 = log(pi[1]) + logpdf(MvNormal(μ1, Σ1), [x, y])
-
-    # Probability under cluster 2
-    logp2 = log(pi[2]) + logpdf(MvNormal(μ2, Σ2), [x, y])
-
-    # Add probabilities using log-sum-exp trick for numeric stability.
-    return logaddexp(logp1, logp2)
+function prob_cluster(x, y, π, μ1, Σ1)
+    "Weighted probability under Gaussian"
+    return π*pdf(MvNormal(μ1, Σ1), [x, y])
 end
 
-function plot_posteriors(X, μ1, μ2, Σ1, Σ2; x1=range(-5, stop=3), x2=range(-5, stop=4))
+function plot_posteriors(X, μ1, μ2; Σ1=[1. 0.;0. 1.], Σ2=[1. 0.;0. 1.], π=[0.5, 0.5], x1=range(-5, stop=3), x2=range(-5, stop=4), size=(500, 500))
     "Scatter data and overlay posterior probabilities via contour plot."
 
     # Show the data
-    scatter(X[:,1], X[:,2], legend=false, title="Synthetic Dataset")
+    scatter(X[:,1], X[:,2], 
+            label="observations", 
+            color="black", 
+            xlims=[x1[1] x1[end]], 
+            ylims=[x2[1] x2[end]], 
+            xlabel=L"$x_1$", 
+            ylabel=L"$x_2$", 
+            grid=false, 
+            size=size)
 
-    # Overlay the posterior probability
-    contour!(x1, x2, (x, y) -> predict(x, y, [0.5, 0.5], μ1, μ2, Σ1, Σ2))
+    # Overlay the posterior probabilities for each cluster
+    contour!(x1, x2, (x, y) -> prob_cluster(x, y, π[1], μ1, Σ1), color="red", label="C1", cbar=nothing)
+    contour!(x1, x2, (x, y) -> prob_cluster(x, y, π[2], μ2, Σ2), color="blue", label="C2", cbar=nothing)
 end
+
+function mean_chain(chain::Chains, param::Symbol)
+    "Quick function to extract sample average from MCMCChains object"
+    return vec(convert(Array{Float64,2}, mean(chain[param].value.data, dims=[1, 3])[:,:,1]))
+end;
