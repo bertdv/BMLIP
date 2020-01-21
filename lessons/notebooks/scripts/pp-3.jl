10789bb5ea0c8872301ac5573477b6793890bf3a
wmkouw <wmkouw@gmail.com>
2020-01-21 12:20:55 +0100

    Worked on polishing pp-3-sampling

--- lessons/notebooks/scripts/pp-3.jl
+++ lessons/notebooks/scripts/pp-3.jl
@@ -15,3 +15,8 @@ function one_hot(index::Number, length_vector::Integer)
     
     return encoded    
 end
+
+function mean_chain(chain::Chains, param::Symbol)
+    "Quick function to extract sample average from MCMCChains object"
+    return vec(convert(Array{Float64,2}, mean(chain[param].value.data, dims=[1, 3])[:,:,1]))
+end;
