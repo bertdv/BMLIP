e3bae331435fdcdc3cede9d3ae3fcae4048369ce
wmkouw <wmkouw@gmail.com>
2020-01-22 16:27:02 +0100

    Finished polishing pp-3-sampling. Synchronized notebooks a bit.

--- lessons/notebooks/scripts/pp-3.jl
+++ lessons/notebooks/scripts/pp-3.jl
@@ -4,7 +4,7 @@ Wouter Kouw
 Last update: 07-01-2020
 """
 
-function one_hot(index::Number, length_vector::Integer)
+function one_hot(index::Number, length_vector::Integer; nonzero::Bool=false)
     "Map integer to one-hot encoding"
     
     # Initialize encoded vector
@@ -13,10 +13,25 @@ function one_hot(index::Number, length_vector::Integer)
     # Set i-th index to 1
     encoded[index] = 1.0
     
+    # Check for strictly positive vector
+    if nonzero
+        
+        # Add epsilon
+        encoded .+= 1e-8
+        
+        # Re-normalize to 1
+        encoded ./= sum(encoded)
+    end
+    
     return encoded    
 end
 
 function mean_chain(chain::Chains, param::Symbol)
     "Quick function to extract sample average from MCMCChains object"
     return vec(convert(Array{Float64,2}, mean(chain[param].value.data, dims=[1, 3])[:,:,1]))
+end
+
+function median_chain(chain::Chains, param::Symbol)
+    "Quick function to extract median from MCMCChains object"
+    return vec(convert(Array{Float64,2}, median(chain[param].value.data, dims=[1, 3])[:,:,1]))
 end;
